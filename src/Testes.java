928d50cdeed8b5428064a9c6deb0c88226a62582
Guilherme Monteiro <guilherme.montnezes@gmail.com>
2022-01-18 19:08:40 -0300

    Removendo imports desnecessários e organizando ambiente de teste.

--- src/Testes.java
+++ src/Testes.java
@@ -1,37 +1,6 @@
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Testes {
     public static void main(String[] args) {
 
-        //Interface.telaInicial(false, null);
-
-        Map<Integer, Candidato> candidatos = new HashMap<>();
-        Candidato jonas = new Candidato("Jonas");
-        Candidato marcos = new Candidato("Marcos");
-        Candidato adrian = new Candidato("adrian");
-        Candidato bia = new Candidato("bia");
-        Candidato gui = new Candidato("gui");
-
-        candidatos.put(1, jonas);
-        candidatos.put(2, marcos);
-        candidatos.put(3, bia);
-        candidatos.put(4, adrian);
-        candidatos.put(5, gui);
-
-        gui.addVoto();
-        gui.addVoto();
-        gui.addVoto();
-
-        jonas.addVoto();
-        jonas.addVoto();
-        jonas.addVoto();
-        jonas.addVoto();
-        jonas.addVoto();
-        jonas.addVoto();
-
-
-
-
+        Interface.telaInicial(false, null);
     }
 }
